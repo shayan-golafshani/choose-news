e16baa4282bcd3171090b2c9844ca2b624d414d3
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-05 16:06:30 -0600

    Remove comments

--- src/App.js
+++ src/App.js
@@ -7,21 +7,15 @@ function App() {
   const [mainPoem, setMainPoem] = useState([]);
   const [errMessage, setErrMessage] = useState('');
 
-  const getAffirmation = () => {   
+  const getPoem = () => {   
       fetch('https://poetrydb.org/title/Ozymandias/lines')
       .then(response => response.json())
       .then(jsondata => setMainPoem(jsondata[0].lines))
       .catch(err => console.error(err));
-    
-    //   fetch('https://www.affirmations.dev/', { 
-    //     mode: 'no-cors', // 'cors' by default
-    // }).then(response =>  response.json())
-    //   .then(data => console.log(data))
 }
     
-    useEffect(() => {
-    getAffirmation()    
-    
+  useEffect(() => {
+    getPoem()    
   },[])
 
   return (
