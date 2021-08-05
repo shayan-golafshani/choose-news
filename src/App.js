cbe88a6825ae0ba2fc20e908b545358d421dba0d
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-05 16:05:08 -0600

    Get poem fetch call working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 //import logo from './logo.svg';
-import { response } from 'express';
-import {useEffect, useState} from 'react';
+//import { response } from 'express';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  const [affirmation, setAffirmation] = useState('');
+  const [mainPoem, setMainPoem] = useState([]);
   const [errMessage, setErrMessage] = useState('');
 
-
-  const getAffirmation = () => {
-    fetch("https://www.affirmations.dev")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
-  }
-  
+  const getAffirmation = () => {   
+      fetch('https://poetrydb.org/title/Ozymandias/lines')
+      .then(response => response.json())
+      .then(jsondata => setMainPoem(jsondata[0].lines))
+      .catch(err => console.error(err));
+    
+    //   fetch('https://www.affirmations.dev/', { 
+    //     mode: 'no-cors', // 'cors' by default
+    // }).then(response =>  response.json())
+    //   .then(data => console.log(data))
+}
+    
+    useEffect(() => {
+    getAffirmation()    
+    
+  },[])
 
   return (
     <div className="App">
