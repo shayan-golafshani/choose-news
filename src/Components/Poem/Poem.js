c04b989a827b6e360d18bf8cbab7207ce3a94b9c
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-06 10:52:27 -0600

    Start layout of poem.js

---
+++ src/Components/Poem/Poem.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import { Route, Switch, NavLink, Link } from 'react-router-dom';
+
+//import ReactPlayer from "react-player"
+//import { tracks } from '../../data/tracks'
+import './App.css';
+
+
+function Poem() {
+  
+  const [allPoems, setAllPoems] = useState([]);
+  const [errMessage, setErrMessage] = useState('');
+
+  const getPoem = () => {   
+      fetch('https://poetrydb.org/random')
+      .then(response => response.json())
+      .then(jsondata => {
+        
+        console.log(jsondata)
+        setMainPoem(jsondata[0].lines)})
+      .catch(err => console.error(err));
+}
+
+  useEffect(() => {
+    getPoem()
+  },[])
+
+  let poem = mainPoem.map((line, index) => {
+    return  <p key={index}>
+              {line}
+            </p>
+  }) 
+
+  return (
+    
+    {/*
+        <PoemCard >
+
+    </PoemCard>
+     */}
+  );
+}
+
+export default App;
