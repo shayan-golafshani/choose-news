11f6f79a06bee947743747ca2d6911f3b43203ec
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-07 12:49:15 -0600

    Set up poem card for feeding data down

--- src/Components/PoemCard/PoemCard.js
+++ src/Components/PoemCard/PoemCard.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const PoemCard = () => {
-
+const PoemCard = ({title, author, lines, linecount, index}) => {
+//pull out the data going to be passed down through state
 
 
 
     return (
-        <section className='poem-card'>
-
+        <section
+        key={index}
+        className='poem-card'>
+            <h3>{title}</h3>
+            <p>By {author}</p>
+            <p>{lines}</p>
+            <p>{linecount}</p>
         </section>
     )
 }
