71fb85ab60397d05a4095900d91e8818f8230312
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-07 16:00:10 -0600

    Add component for displaying favorited  cards, structure like Poem.js

---
+++ src/Components/FavoritePoems/FavoritePoems.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import PoemCard from '../PoemCard/PoemCard';
+//use same css as before
+import './Poems.css';
+
+function FavoritePoems() {
+  
+  const [favoritePoems, setFavoritePoems] = useState(JSON.parse(localStorage.getItem('favePoems')) || []);
+  const [errMessage, setErrMessage] = useState('');
+
+
+  let poetryCards = favoritePoems.map((poem, index) => {
+    return <PoemCard
+            index={index}
+            title={poem.title}
+            author={poem.author}
+            lines={poem.lines}
+            linecount={poem.linecount}
+            key={index}
+           />
+  })
+
+  return (
+    <>
+    <section className='poetry-container'>
+      {!!poetryCards.length && poetryCards}
+    </section>
+    </>
+  );
+}
+
+export default FavoritePoems;
