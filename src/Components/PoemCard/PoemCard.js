7ae9ea44ad32d1d61617b027e8562fe9365e172f
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-07 13:17:51 -0600

    Add setup for showing a random image w/ poetry

--- src/Components/PoemCard/PoemCard.js
+++ src/Components/PoemCard/PoemCard.js
@@ -3,7 +3,8 @@ import React from 'react';
 const PoemCard = ({title, author, lines, linecount, index}) => {
 //pull out the data going to be passed down through state
 
-
+//figure out how to make it display a different image everytime!
+<img src='http://thisartworkdoesnotexist.com' alt='random AI generated artwork from thisartworkdoesnotexist.com'/>
 
     return (
         <section
