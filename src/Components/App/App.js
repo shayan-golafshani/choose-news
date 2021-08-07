b80cfac0d78e8472055db61780f90c67a204d311
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-06 20:22:04 -0600

    Remove unnecessary link

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { Route, Switch, NavLink, Link } from 'react-router-dom';
+import { Route, Switch, NavLink } from 'react-router-dom';
 
 import ReactPlayer from "react-player"
 import { tracks } from '../../data/tracks'
