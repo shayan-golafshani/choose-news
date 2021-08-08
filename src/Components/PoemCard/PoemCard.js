b2bacccc03e1bcb05d167ff928c0d06ef261187b
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-07 18:30:47 -0600

    Pass down prop for conditionally rendering fav/delete btn

--- src/Components/PoemCard/PoemCard.js
+++ src/Components/PoemCard/PoemCard.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './PoemCard.css';
 
-const PoemCard = ({title, author, lines, linecount, index, addToFaves}) => {
+const PoemCard = ({title, author, lines, linecount, index, addToFaves, buttonType}) => {
 //pull out the data going to be passed down through state
 
 //figure out how to make it display a different image everytime!
-<img src='http://thisartworkdoesnotexist.com' alt='random AI generated artwork from thisartworkdoesnotexist.com'/>
+//<img src='http://thisartworkdoesnotexist.com' alt='random AI generated artwork from thisartworkdoesnotexist.com'/>
+
+let buttonImg = buttonType ?
+                   <img id={index} src="https://img.icons8.com/office/30/000000/like--v1.png" alt="favorite button"/>
+                 : <img id={index} src="https://img.icons8.com/office/30/000000/delete-sign.png" alt="delete button"/>;
+
 
 let spacedLines = lines.map((line, index) => {
     return <p key={index}>{line}</p>
@@ -17,7 +22,7 @@ let spacedLines = lines.map((line, index) => {
                <button 
                onClick={e => addToFaves(e)}
                className="favorite-btn">
-                    <img id={index} src="https://img.icons8.com/office/30/000000/like--v1.png"/>
+                    {buttonImg}
                </button>
             <h3>{title}</h3>
             <p>By {author}</p>
