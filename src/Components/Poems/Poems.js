d3738fcc9945eaf4cb8dc04f21b68b7eb97da408
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-08 12:48:27 -0600

    Clean up console.logs

--- src/Components/Poems/Poems.js
+++ src/Components/Poems/Poems.js
@@ -8,7 +8,6 @@ function Poem() {
   const [selectedAuthor, setSelectedAuthor] = useState('');
   const [selectedAuthorPoems , setSelectedAuthorPoems] = useState([]);
   // not using this state yet.
-  //const [allPoems, setAllPoems] = useState([]);
   const [errMessage, setErrMessage] = useState('');
 
 
@@ -17,28 +16,15 @@ function Poem() {
     
     if(localData) {
       localData = JSON.parse(localData)
-      //console.log("THIS IS LOCAL DATA inside add2Faves", localData)
     }
-    
     let matchedPoem = selectedAuthorPoems.filter((poem, index) => index === parseInt(e.target.id))
-    
-    //matchedPoem = matchedPoem[0]
-
-    //console.log("This is the matched poem", matchedPoem);
-    //let stringifiedPoem = JSON.stringify(matchedPoem[0])
 
     if(localData) {
-      //localData = JSON.parse(localData)
-      console.log("LOCAL DATA inside Assignment IF", localData)
-
       let found = localData.find(poem => poem.title === matchedPoem[0].title)
-
-      console.log("This is the value of found", found)
 
       if(!found) {
         localData.push(matchedPoem[0])
-        //stringify it
-        //console.log('LOCAL DATA AFTER Pushing matched poem', localData)
+        
         localData = JSON.stringify(localData)
         localStorage.setItem('favePoems', localData);
         return true;
@@ -57,7 +43,7 @@ function Poem() {
       .then(response => response.json())
       .then(jsondata => {
         
-        console.log(jsondata, 'inside GET ALL AUTHORS fetch')
+        //console.log(jsondata, 'inside GET ALL AUTHORS fetch')
         setAllAuthors(jsondata.authors)})
       .catch(err => console.error(err));
   }
@@ -68,13 +54,13 @@ function Poem() {
   
   useEffect(() => {
     if(selectedAuthor){
-      console.log('SELECTED AUTHOR inside useEFFECT', selectedAuthor)
+      //console.log('SELECTED AUTHOR inside useEFFECT', selectedAuthor)
       const getAuthorPoems = () => {   
         fetch(`https://poetrydb.org/author/${selectedAuthor}`)
         .then(response => response.json())
         .then(jsondata => {
           
-          console.log(jsondata, "ALL AUTHOR POEMS")
+          //console.log(jsondata, "ALL AUTHOR POEMS")
           setSelectedAuthorPoems(jsondata)})
         .catch(err => console.error(err));
     }
@@ -82,10 +68,9 @@ function Poem() {
     }
   }, [selectedAuthor])
 
-  
   let options = allAuthors.map((author, index) => <option key={index} value={author}>{author}</option>)
-  let poetryCards = selectedAuthorPoems.map((poem, index) => {
 
+  let poetryCards = selectedAuthorPoems.map((poem, index) => {
     return <PoemCard
             addToFaves={addToFaves}
             author={poem.author}
@@ -106,9 +91,9 @@ function Poem() {
         onChange={e => setSelectedAuthor(e.target.value)}>
         {options}
       </select>
-    <section className='poetry-container'>
-      {!!poetryCards.length && poetryCards}
-    </section>
+      <section className='poetry-container'>
+        {!!poetryCards.length && poetryCards}
+      </section>
     </>
   );
 }
