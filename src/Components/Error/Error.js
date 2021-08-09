6f5625525983eccfdc4f8dcf04140151fa23a51f
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-09 07:28:25 -0600

    Reorder error componenet messages

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -6,11 +6,11 @@ const Error = ({message}) => {
 
     return (
         <div className='error'>         
-                <h3>{message}</h3>
-            <Link to ='/'>
                 <h4 className='error-message'>Errors are red, blue-screens of death are blue, Sorry we were unable to find this resource for you!</h4>
+                <h5>{message}</h5>
+            <Link to ='/'>
+                <img className='error-image' id='error-image' src='https://media.giphy.com/media/3osxY9kuM2NGUfvThe/giphy.gif' alt='roses are red violets are blue, here is an unexpected error for you!'/>
             </Link>
-            <img className='error-image' id='error-image' src='https://media.giphy.com/media/j9XoexYMmd7LdntEK4/giphy.gif' alt='roses are red violets are blue, here is an unexpected error for you!'/>
         </div>
     )
 }
