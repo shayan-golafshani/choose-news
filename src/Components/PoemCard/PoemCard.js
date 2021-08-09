79e95b415667465ad0f8be42e859b7e19ec881ce
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-09 09:35:31 -0600

    remove unnecessary import

--- src/Components/PoemCard/PoemCard.js
+++ src/Components/PoemCard/PoemCard.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import PropTypes, { string } from 'prop-types';
+import PropTypes from 'prop-types';
 import './PoemCard.css';
 
 const PoemCard = ({title, author, lines, linecount, index, addToFaves, buttonType, removeFromFaves}) => {
