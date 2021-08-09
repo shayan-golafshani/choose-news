cde12d665dfbfdca32f5f01e59cda7b3aa57f62b
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-09 14:57:29 -0600

    Set up error handling for  failed network requests

--- cypress/integration/errror_handling_spec.js
+++ cypress/integration/errror_handling_spec.js
@@ -1,22 +1,19 @@
 describe('Error Handling', () => {
-
     beforeEach(() => {
         cy.intercept('https://poetrydb.org/random', {statusCode: 500})
         cy.intercept('https://poetrydb.org/author', {statusCode: 500})
         cy.intercept('https://poetrydb.org/author/Henry David Thoreau')
     })
 
-    it('Should display error message for 500 status code', () => {
-        
+    it('Should display error message for 500 status code', () => {      
           cy.visit('http://localhost:3000/')
           .contains('Errors are red, blue-screens of death are blue, Sorry we were unable to find this resource for you!')
       });
 
 
     it('Should display error message for 500 status code', () => {
-      
         cy.visit('http://localhost:3000/poems')
-        // .contains('Oops server is down! Please try again later.')
+        .contains('Errors are red, blue-screens of death are blue, Sorry we were unable to find this resource for you!')
     });
 
 });
