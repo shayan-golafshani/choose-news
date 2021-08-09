c2682808cd0ab04be267f07fe11f4d27983e90e6
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-09 16:03:07 -0600

    Test buttons display

--- cypress/integration/main_page_spec.js
+++ cypress/integration/main_page_spec.js
@@ -8,11 +8,14 @@ describe('Feeling lucky page', () => {
     })
 
     it('Should contain header with zenbox text' , ()=> {
-        cy.contains('Tha-Zen');
+        cy.contains('.title-text', 'Tha-Zen');
+        // cy.get('.title-text')
     })
 
-    it('Should contain header 3 buttons inside the header' , ()=> {
-
+    it('Should contain header 3 buttons inside the header', () => {
+        cy.get('.active > button').should('contain', 'Feeling lucky!')
+        cy.get('[href="/poems"] > button').should('contain', 'Search poems')
+        cy.get('[href="/favorited-poems"] > button').should('contain', 'Fave poems')
     })
 
     it('Should display a rotating piece of art' , ()=> {
