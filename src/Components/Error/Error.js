4328145418bb26d9baf9a21f01a98ab3cfc6e12f
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-09 02:45:23 -0600

    Finish setup of error component

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,19 +1,17 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
 import './Error.css'
-// import '../Resources/tomato1.png'
 
 const Error = ({message}) => {
 
     return (
         <div className='error'>         
+                <h3>{message}</h3>
             <Link to ='/'>
-                <h3 className='error-message'>Something went wrong. Click here to be redirected.</h3>
+                <h4 className='error-message'>Errors are red, blue-screens of death are blue, Sorry we were unable to find this resource for you!</h4>
             </Link>
-            <img className='error-image' id='error-image' src='https://media.giphy.com/media/KXBtTtm3kB8BO/giphy.gif' alt='roses are red violets are blue, here is an unexpected error for you!'/>
+            <img className='error-image' id='error-image' src='https://media.giphy.com/media/j9XoexYMmd7LdntEK4/giphy.gif' alt='roses are red violets are blue, here is an unexpected error for you!'/>
         </div>
     )
-
 }
-
 export default Error;
