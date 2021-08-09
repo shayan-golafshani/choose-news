b69b9b266cf4e4edd6449cbec3a1d25a67592265
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-09 16:58:31 -0600

    Add stub for poems and check that the poems are likeable

--- cypress/integration/poem_selection_spec.js
+++ cypress/integration/poem_selection_spec.js
@@ -4,6 +4,12 @@ describe('Poem selection page spec', () => {
         statusCode: 200,
         fixture: 'authors.json'
         });
+
+        cy.intercept('GET', 'https://poetrydb.org/author/Algernon%20Charles%20Swinburne', {
+            statusCode: 200,
+            fixture: 'poem.json'
+            });
+
         cy.visit('http://localhost:3000/poems')
     })
 
@@ -16,13 +22,31 @@ describe('Poem selection page spec', () => {
     })
 
 
-    it('Should contain a select with 14 authors', () => {
-        cy.intercept('GET', 'https://poetrydb.org/author/Algernon%20Charles%20Swinburne', {
-            statusCode: 200,
-            fixture: 'poem.json'
-            });
-
-        cy.wait(1000);
+    it('Should contain two stubbed poems', () => {
+        // cy.wait(1000);
         cy.get('select').select('Algernon Charles Swinburne')
+        cy.get('.poetry-container > :nth-child(1) > h3').contains("On the Death of Robert Browning")
+        cy.get('.poetry-container > :nth-child(2)').contains('Etude Realiste')
     })
+
+    it('Each stubbed poem should be likeable', () => {
+        // cy.wait(1000);
+        cy.get('select').select('Algernon Charles Swinburne')
+        cy.get('.favorite-btn').click({ multiple: true })
+        cy.get(':nth-child(1) > .disabled-favorite').should('have.css', 'background-color')
+        .and('eq', 'rgb(255, 0, 0)')
+        cy.get(':nth-child(2) > .disabled-favorite').should('have.css', 'background-color')
+        .and('eq', 'rgb(255, 0, 0)')
+    })
+
+    it('Each stubbed poem should show up on favorites page', () => {
+        
+        cy.get('select').select('Algernon Charles Swinburne')
+        cy.get('.favorite-btn').click({ multiple: true })
+        cy.get(':nth-child(1) > .disabled-favorite').should('have.css', 'background-color')
+        .and('eq', 'rgb(255, 0, 0)')
+        cy.get(':nth-child(2) > .disabled-favorite').should('have.css', 'background-color')
+        .and('eq', 'rgb(255, 0, 0)')
+    })
+
 })
