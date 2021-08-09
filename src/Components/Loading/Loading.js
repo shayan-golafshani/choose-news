828645a8b607545fe4775a44c6a3be6931083ab4
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-09 04:06:17 -0600

    update alt

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -6,7 +6,7 @@ const Loading = () => {
     return (
         <div className='loading'>
             <h2 className='loading-message'>Loading ...</h2>
-            <img className='loading-image' id='loading-1' src='https://media.giphy.com/media/lviKnSMZfNdDWDthXs/giphy.gif' alt='loading image'/>      
+            <img className='loading-image' id='loading-1' src='https://media.giphy.com/media/lviKnSMZfNdDWDthXs/giphy.gif' alt='loading spinner'/>      
         </div>
     )
 }
