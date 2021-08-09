cccf91705ee6c28dcb6a59c437678f462262b47d
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-09 16:41:13 -0600

    Test select & stub Algernon's poem network request

--- cypress/integration/poem_selection_spec.js
+++ cypress/integration/poem_selection_spec.js
@@ -4,14 +4,25 @@ describe('Poem selection page spec', () => {
         statusCode: 200,
         fixture: 'authors.json'
         });
-        cy.visit('http://localhost:3000/poem')
+        cy.visit('http://localhost:3000/poems')
     })
 
     it('Should contain header with zenbox text' , ()=> {
-        cy.contains('Tha-Zen');
+        cy.contains('.title-text', 'Tha-Zen');
     })
 
-    it('Should contain blah', () => {
-        
+    it('Should contain a select with 14 authors', () => {
+        cy.get('select').select('Algernon Charles Swinburne')
+    })
+
+
+    it('Should contain a select with 14 authors', () => {
+        cy.intercept('GET', 'https://poetrydb.org/author/Algernon%20Charles%20Swinburne', {
+            statusCode: 200,
+            fixture: 'poem.json'
+            });
+
+        cy.wait(1000);
+        cy.get('select').select('Algernon Charles Swinburne')
     })
 })
