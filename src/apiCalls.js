c38e40d563a6a0e17588b1371c8fe6ab8328b528
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-22 11:14:31 -0600

    Remove superfluous fetch calls and refactor name

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
 import { checkForError} from './util';
 
-export function randomPoem(){
+export function randomArticles(){
     return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
       .then(response => checkForError(response))
 }
@@ -8,14 +8,4 @@ export function randomPoem(){
 export const getArticlesByCat = (cat) => {
   return fetch(`https://api.nytimes.com/svc/topstories/v2/${cat}.json?api-key=${process.env.REACT_APP_API_KEY}`)
   .then(response => checkForError(response))
-}
-
-export function getAllAuthors(){
-    return fetch('https://poetrydb.org/author')
-      .then(response => checkForError(response))
-}
-
-export function getPoemsByAuthor(selectedAuthor){
-    return fetch(`https://poetrydb.org/author/${selectedAuthor}`)
-    .then(response => checkForError(response))
 }
