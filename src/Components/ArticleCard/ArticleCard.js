e1beb401474f37c468e4e0bc2797e23731cc08a7
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-24 09:40:37 -0600

    Remove comments

--- src/Components/ArticleCard/ArticleCard.js
+++ src/Components/ArticleCard/ArticleCard.js
@@ -1,17 +1,8 @@
-import React, {useState} from 'react';
-//import PropTypes from 'prop-types';
+import React from 'react';
+
 import './ArticleCard.css';
 
-// abstract={article.abstract}
-// byline={article.byline}
-// date={article.published_date}
-// multimedia={article.multimedia}
-// title={article.title}
-
-const ArticleCard = ({abstract, byline, date, multimedia, title, index}) => {
-
-
-const handleClick = (e) => {}
+const ArticleCard = ({abstract, byline, date, multimedia, title, index, url}) => {
 
     return (
         <section
@@ -19,14 +10,11 @@ const handleClick = (e) => {}
             className='article-card'>
             <h3> {title} </h3>
             <p> {byline} </p>
-            <p> {date} </p>
+            <p> date : {date} </p>
             <p> {abstract} </p>
-            {/* <p>{mainArticle.section}</p>
-        <p>{mainArticle.subsection}</p>
-        <p className='article-title'>Title: <a href={mainArticle.url}>{mainArticle.title}  </a></p>
-        <ImageGallery items={images}/>
-        <p>Title: {mainArticle.title}</p>
-        <p>{mainArticle.abstract}</p> */}
+            <a href={url}>  
+                <button> NYT Page</button>
+            </a>
         </section>
     )
 }
